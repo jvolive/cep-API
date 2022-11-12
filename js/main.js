bbbf57a64d322d1c08b0c7b015913a67fe487f05
João Oliveira <joaooliveira1999@outlook.com>
2022-11-12 18:20:33 -0300

    CEP puxando a rua

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 "use strict";
 
-const pesquisarCep = () => {
+const preencherFormulario = (endereco) => {
+  document.getElementById("endereco").value = endereco.logradouro;
+};
+
+const pesquisarCep = async () => {
   const cep = document.getElementById("cep").value;
   const url = `http://viacep.com.br/ws/${cep}/json/`;
-  fetch(url)
-    .then((responde) => responde.json())
-    .then(console.log);
+
+  const dados = await fetch(url);
+  const endereco = await dados.json();
+  preencherFormulario(endereco);
 };
 
 document.getElementById("cep").addEventListener("focusout", pesquisarCep);
