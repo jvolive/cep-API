dc7184c33458345aaeceabc5bb28ad0ee0c14d9b
João Oliveira <joaooliveira1999@outlook.com>
2022-11-13 17:07:12 -0300

    CEP validação

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 "use strict";
 
+const limpaFormulario = (endereco) => {
+  document.getElementById("endereco").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("estado").value = "";
+};
+
 const preencherFormulario = (endereco) => {
+  limpaFormulario();
   document.getElementById("endereco").value = endereco.logradouro;
   document.getElementById("bairro").value = endereco.bairro;
   document.getElementById("cidade").value = endereco.localidade;
